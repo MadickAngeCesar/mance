abec045f09338ab2250a4723f4195f3e367f5744
Madick Ange César <129757922+MadickAngeCesar@users.noreply.github.com>
2026-05-07 10:47:05 +0100

    Add TawkTo component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../components/globals.css";
 import { LanguageProvider } from "@/components/i18n/language-provider";
+import TawkTo from "@/components/TawkTo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <LanguageProvider>{children}</LanguageProvider>
+        <TawkTo />
       </body>
     </html>
   );
